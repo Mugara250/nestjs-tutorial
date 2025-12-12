1fd7bf01b9042441e2527f0c343eb0c5f52c5ef0
Mugara250 <mbonyumugara52@gmail.com>
2025-12-12 18:32:01 +0200

    get users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import fsp from 'node:fs/promises';
+import { z } from 'zod';
+
+const UserSchema = z.object({
+  _id: z.string(),
+  name: z.string(),
+  email: z.string(),
+  role: z.string(),
+  active: z.boolean(),
+  photo: z.string(),
+  password: z.string(),
+});
+
+type UserDTO = z.infer<typeof UserSchema>;
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users: UserDTO[] = [];
+
+  constructor() {
+    void this.loadUsers();
+  }
+  private async loadUsers() {
+    try {
+      const data = await fsp.readFile(
+        `${process.cwd()}/dev-data/data/users.json`,
+        'utf8',
+      );
+      this.users = z.array(UserSchema).parse(JSON.parse(data));
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  getUsers() {
+    return this.users;
+  }
+}
